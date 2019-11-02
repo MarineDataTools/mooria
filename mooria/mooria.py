5a8609a447d30b90c9b119bfc4b63e2ca53ebc3e
Peter Holtermann <peter.holtermann@io-warnemuende.de>
2019-11-02 21:57:32 +0100

    Begin of mooring design gui

--- mooria/mooria.py
+++ mooria/mooria.py
@@ -43,7 +43,44 @@ from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as Navigatio
 from matplotlib.figure import Figure
 
 
+class mainWidget(QtWidgets.QWidget):
+    def __init__(self,logging_level=logging.INFO,within_qgis = False):
+        QtWidgets.QWidget.__init__(self)        
+        self.moorings = []
+        mooring = self.create_mooring_widget()
+        # Tabs
+        self.tabs = QtWidgets.QTabWidget()
+        self.tabs.addTab(mooring['widget'],'Mooring')
+        self.layout = QtWidgets.QGridLayout(self)
+        self.layout.addWidget(self.tabs,2,0,1,2)        
 
+
+    def create_mooring_widget(self):
+        mooring = {}
+        mooring['widget']     = QtWidgets.QWidget()
+        mooring['layout']     = QtWidgets.QGridLayout(mooring['widget'])
+        mooring['devtable']   = QtWidgets.QTableWidget() # Table with all available devices to choose from
+        mooring['devwidget']  = QtWidgets.QWidget() # Special widget to enter parameters for that device
+        mooring['moortable']  = QtWidgets.QTableWidget()
+        mooring['layout'].addWidget(mooring['moortable'],0,0)
+        mooring['layout'].addWidget(mooring['devwidget'],0,1)
+        mooring['layout'].addWidget(mooring['devtable'],0,2)
+
+        # Fill the devices table
+        table = mooring['devtable']
+        table.setColumnCount(1)
+        table.setHorizontalHeaderLabels(['Name'])
+        nrows = len(devices)
+        for row,dev in enumerate(devices):
+            item = QtWidgets.QTableWidgetItem( dev )            
+            table.insertRow(row)
+            table.setItem(row,0,item)
+
+        table.resizeColumnsToContents()
+        
+        self.moorings.append(mooring)
+        return mooring
+    
 
 class mooriaMainWindow(QtWidgets.QMainWindow):
     def __init__(self,logging_level=logging.INFO):
